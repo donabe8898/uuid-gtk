fc17a40879dc00e2c198cda1dac404a42d38bbe1
donabe8898 <60559703+donabe8898@users.noreply.github.com>
2023-08-06 22:10:42 +0900

    地味すぎる微修正
    Signed-off-by: donabe8898 <60559703+donabe8898@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,9 @@
-use std::cell::RefCell;
-use std::clone;
-use std::rc::Rc;
-
-use glib_macros::clone;
-use gtk::{gio::ResourceLookupFlags, prelude::*};
+use gtk::prelude::*;
 use uuid::Uuid;
 
 fn main() {
-    let application = gtk::Application::new(Some("com.donabe8898.dev"), Default::default());
+    let application: gtk::Application =
+        gtk::Application::new(Some("com.donabe8898.dev"), Default::default());
     application.connect_activate(build_ui);
     application.run();
 }
@@ -17,7 +13,7 @@ fn build_ui(application: &gtk::Application) {
     window.set_title(Some("UUID gen"));
     window.set_default_size(500, 500);
 
-    let vbox = gtk::Box::builder()
+    let vbox: gtk::Box = gtk::Box::builder()
         .orientation(gtk::Orientation::Vertical)
         .halign(gtk::Align::Center)
         .spacing(10)
@@ -30,15 +26,16 @@ fn build_ui(application: &gtk::Application) {
     // 子へ
     window.set_child(Some(&vbox));
 
-    let uuid_txt = gtk::Text::new();
-    let button = gtk::Button::with_label("Gen UUID");
+    let uuid_txt: gtk::Text = gtk::Text::new();
+    let button: gtk::Button = gtk::Button::with_label("Gen UUID");
 
-    let clone_uuid_txt = uuid_txt.clone();
+    let clone_uuid_txt: gtk::Text = uuid_txt.clone();
     button.connect_clicked(move |_| {
-        let id = Uuid::new_v4();
-        let str_id = id.to_string();
+        let id: Uuid = Uuid::new_v4();
+        let str_id: String = id.to_string();
         clone_uuid_txt.set_text(&str_id);
     });
+
     vbox.append(&uuid_txt);
     vbox.append(&button);
 
